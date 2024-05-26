ea84d4a269badb49573d1954664a28baf504dd83
Tiago Dinis <tiagodinis33@proton.me>
2024-05-26 20:38:11 +0100

    fix: set window size to 0, 0 to let the compositor decide the size of the window

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,10 @@ fn main() {
         .expect("failed to take a screenshot")
         .to_rgba8();
     let (width, height) = screenshot_image.dimensions();
-    let (mut rl, thread) = raylib::init().title(env!("CARGO_BIN_NAME"))
+    let (mut rl, thread) = raylib::init()
+        .title(env!("CARGO_BIN_NAME"))
         .fullscreen()
+        .size(0, 0)
         .transparent()
         .vsync()
         .build();
@@ -38,10 +40,13 @@ fn main() {
         .load_texture_from_image(&thread, &screenshot_image)
         .expect("failed to load screenshot into a texture");
     #[cfg(feature = "dev")]
-    let mut spotlight_shader = rl.load_shader(&thread, None, Some("shaders/spotlight.fs")).expect("Failed to load spotlight shader");
+    let mut spotlight_shader = rl
+        .load_shader(&thread, None, Some("shaders/spotlight.fs"))
+        .expect("Failed to load spotlight shader");
 
     #[cfg(not(feature = "dev"))]
-    let mut spotlight_shader = rl.load_shader_from_memory(&thread, None, Some(include_str!("../shaders/spotlight.fs")));
+    let mut spotlight_shader =
+        rl.load_shader_from_memory(&thread, None, Some(include_str!("../shaders/spotlight.fs")));
     let mut rl_camera = Camera2D::default();
     rl_camera.zoom = 1.0;
     // TODO: Get the current monitor position in virtual space and put the camera there
@@ -50,7 +55,7 @@ fn main() {
     let mut velocity = Vector2::default();
     let mut spotlight_radius_multiplier = 1.0;
     let mut spotlight_radius_multiplier_delta = 0.0;
-    
+
     #[cfg(feature = "dev")]
     let mut spotlight_tint_uniform_location;
     #[cfg(feature = "dev")]
@@ -66,17 +71,22 @@ fn main() {
 
     spotlight_tint_uniform_location = spotlight_shader.get_shader_location("spotlightTint");
     cursor_position_uniform_location = spotlight_shader.get_shader_location("cursorPosition");
-    spotlight_radius_multiplier_uniform_location = spotlight_shader.get_shader_location("spotlightRadiusMultiplier");
+    spotlight_radius_multiplier_uniform_location =
+        spotlight_shader.get_shader_location("spotlightRadiusMultiplier");
     while !rl.window_should_close() {
         if rl.is_mouse_button_down(MouseButton::MOUSE_BUTTON_RIGHT) {
             break;
         }
         #[cfg(feature = "dev")]
         if rl.is_key_pressed(KeyboardKey::KEY_R) {
-            spotlight_shader = rl.load_shader(&thread, None, Some("shaders/spotlight.fs")).expect("Failed to load spotlight shader");
+            spotlight_shader = rl
+                .load_shader(&thread, None, Some("shaders/spotlight.fs"))
+                .expect("Failed to load spotlight shader");
             spotlight_tint_uniform_location = spotlight_shader.get_shader_location("spotlightTint");
-            cursor_position_uniform_location = spotlight_shader.get_shader_location("cursorPosition");
-            spotlight_radius_multiplier_uniform_location = spotlight_shader.get_shader_location("spotlightRadiusMultiplier");
+            cursor_position_uniform_location =
+                spotlight_shader.get_shader_location("cursorPosition");
+            spotlight_radius_multiplier_uniform_location =
+                spotlight_shader.get_shader_location("spotlightRadiusMultiplier");
         }
         let enable_spotlight = rl.is_key_down(KeyboardKey::KEY_LEFT_CONTROL);
         let scrolled_amount = rl.get_mouse_wheel_move_v().y;
@@ -85,7 +95,10 @@ fn main() {
             spotlight_radius_multiplier_delta = -15.0;
         }
         if scrolled_amount != 0.0 {
-            match (enable_spotlight, rl.is_key_down(KeyboardKey::KEY_LEFT_SHIFT)) {
+            match (
+                enable_spotlight,
+                rl.is_key_down(KeyboardKey::KEY_LEFT_SHIFT),
+            ) {
                 (_, false) => {
                     delta_scale += scrolled_amount as f64;
                 }
@@ -98,23 +111,27 @@ fn main() {
         }
         if delta_scale.abs() > 0.5 {
             let p0 = scale_pivot / rl_camera.zoom;
-            rl_camera.zoom =
-                (rl_camera.zoom as f64 + delta_scale * rl.get_frame_time() as f64).clamp(1.0, 10.) as f32;
+            rl_camera.zoom = (rl_camera.zoom as f64 + delta_scale * rl.get_frame_time() as f64)
+                .clamp(1.0, 10.) as f32;
             let p1 = scale_pivot / rl_camera.zoom;
             rl_camera.target += p0 - p1;
             delta_scale -= delta_scale * rl.get_frame_time() as f64 * 4.0
         }
-        spotlight_radius_multiplier =
-            (spotlight_radius_multiplier as f64 + spotlight_radius_multiplier_delta * rl.get_frame_time() as f64).clamp(0.3, 10.) as f32;
-        spotlight_radius_multiplier_delta -= spotlight_radius_multiplier_delta * rl.get_frame_time() as f64 * 4.0;
+        spotlight_radius_multiplier = (spotlight_radius_multiplier as f64
+            + spotlight_radius_multiplier_delta * rl.get_frame_time() as f64)
+            .clamp(0.3, 10.) as f32;
+        spotlight_radius_multiplier_delta -=
+            spotlight_radius_multiplier_delta * rl.get_frame_time() as f64 * 4.0;
         const VELOCITY_THRESHOLD: f32 = 15.0;
         if rl.is_mouse_button_down(MouseButton::MOUSE_BUTTON_LEFT) {
-            let delta = rl.get_screen_to_world2D(rl.get_mouse_position() - rl.get_mouse_delta(), rl_camera) - rl.get_screen_to_world2D(rl.get_mouse_position(), rl_camera);
+            let delta = rl
+                .get_screen_to_world2D(rl.get_mouse_position() - rl.get_mouse_delta(), rl_camera)
+                - rl.get_screen_to_world2D(rl.get_mouse_position(), rl_camera);
             rl_camera.target += delta;
             velocity = delta * rl.get_fps().as_f32();
-        } else if velocity.length_sqr() > VELOCITY_THRESHOLD*VELOCITY_THRESHOLD {
+        } else if velocity.length_sqr() > VELOCITY_THRESHOLD * VELOCITY_THRESHOLD {
             rl_camera.target += velocity * rl.get_frame_time();
-            velocity -= velocity * rl.get_frame_time()*6.0;
+            velocity -= velocity * rl.get_frame_time() * 6.0;
         }
 
         let mut d = rl.begin_drawing(&thread);
@@ -122,11 +139,23 @@ fn main() {
         if enable_spotlight {
             mode2d.clear_background(SPOTLIGHT_TINT);
             let mouse_position = mode2d.get_mouse_position();
-            spotlight_shader.set_shader_value(spotlight_tint_uniform_location, SPOTLIGHT_TINT.color_normalize());
+            spotlight_shader.set_shader_value(
+                spotlight_tint_uniform_location,
+                SPOTLIGHT_TINT.color_normalize(),
+            );
             let screen_height = mode2d.get_screen_height().as_f32();
-            spotlight_shader.set_shader_value(cursor_position_uniform_location, Vector2::new(mouse_position.x, screen_height + screen_height / 1.65 - mouse_position.y));
-            spotlight_shader.set_shader_value(spotlight_radius_multiplier_uniform_location, spotlight_radius_multiplier);
-            
+            spotlight_shader.set_shader_value(
+                cursor_position_uniform_location,
+                Vector2::new(
+                    mouse_position.x,
+                    screen_height + screen_height / 1.65 - mouse_position.y,
+                ),
+            );
+            spotlight_shader.set_shader_value(
+                spotlight_radius_multiplier_uniform_location,
+                spotlight_radius_multiplier,
+            );
+
             let mut shader_mode = mode2d.begin_shader_mode(&spotlight_shader);
             shader_mode.draw_texture(&screenshot_texture, 0, 0, Color::WHITE);
         } else {
